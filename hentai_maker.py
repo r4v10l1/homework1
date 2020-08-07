ba59f3abebc715694bb86fa776a3b3f4b79c8664
ravi0li <mikel.veg777@gmail.com>
2020-08-08 00:28:38 +0200

    JUST HOMEWORK NOTHING SPECIAL
    Added more filetypes and more URL formats, learn to code, cunt

--- hentai_maker.py
+++ hentai_maker.py
@@ -22,6 +22,7 @@ def banner():
         print()
         print(" %s%s[!] Error. Invalid operative system, exiting...%s" % (Style.BRIGHT, Fore.RED, Style.RESET_ALL))
         print()
+
     print()
     print(" %s%s  ::   .: .,:::::::::.    :::.:::::::::::::::.     :::" % (Style.BRIGHT, Fore.WHITE))
     print("  ,;;   ;;,;;;;''''`;;;;,  `;;;;;;;;;;;'''';;`;;    ;;;  dynasty-scans.com")
@@ -42,19 +43,36 @@ except KeyboardInterrupt:
     exit(1)
 
 if URL[-1] == "/":
-    URL = URL[:-1]
+    URL = URL[:-1].strip()
 
 if "/" not in URL:
     print(" %s%s[!] Error. Invalid URL input, exiting...%s" % (Style.BRIGHT, Fore.RED, Style.RESET_ALL))
     print()
     exit(1)
-elif ".jpg" in URL.split("/")[-1]:
-    URL = "/".join(URL.split("/")[0:-1]) + "/"
-elif ".jpg" not in URL.split("/")[-1]:
+if "#" in URL:
+    image_format = True
+    complex = True
+    str_len = int(URL.split("/")[-1].split("#")[1].split(".")[0])
+    if "jpg" in URL:
+        image_type = ".jpg"
+    elif "png" in URL:
+        image_type = ".png"
+elif "jpg" or "png" in URL.split("/")[-1]:
+    image_format = True
+    complex = False
+    str_len = int(len(URL.split("/")[-1].split(".")[0]))
+    if "jpg" in URL:
+        image_type = ".jpg"
+    elif "png" in URL:
+        image_type = ".png"
+elif "jpg" or "png" not in URL.split("/")[-1]:
+    image_format = False
+    image_type = ".jpg"
     URL = URL + "/"
+    str_len = 4
 
 try:
-    number = 0
+    number2fill = 0
     if operative_system == "win":
         os.system("if exist \"u_temp\" rmdir/q/s u_temp")
         os.system("if not exist \"u_temp\" mkdir u_temp")
@@ -66,20 +84,33 @@ try:
         print(" %s%s[!] Error. Invalid operative system, exiting...%s" % (Style.BRIGHT, Fore.RED, Style.RESET_ALL))
         print()
     while True:
-        number = int(number) + 1
-        number = str(number).zfill(4)
-        THIS_IMG = "https://dynasty-scans.com" + URL + number + ".jpg"
+        number2fill = int(number2fill) + 1
+        number = str(number2fill).zfill(str_len)
+        if not image_format:
+            THIS_IMG = "https://dynasty-scans.com" + URL + number + ".jpg"
+        elif image_format:
+            if complex:
+                THIS_IMG = "https://dynasty-scans.com" + "/".join(URL.split("/")[:-1]) + "/" + URL.split("/")[-1].split("#")[0] + number + image_type
+            elif not complex:
+                THIS_IMG = "https://dynasty-scans.com" + "/".join(URL.split("/")[:-1]) + "/" + number + image_type
+        ## DEBUG ##
+        # print(THIS_IMG)
+        # input()
+        # exit(1)
         try:
             r = requests.get(THIS_IMG, allow_redirects=True)
-            if r.headers["content-type"] not in "image/jpeg":
+            images = {"image/jpeg", "image/png"}
+            if r.headers["content-type"] not in images:
                 print()
-                print(" %s%s[+]%s All done! Exiting...%s" % (Style.BRIGHT, Fore.GREEN, Fore.WHITE, Style.RESET_ALL))
-                input(" Enter...")
+                if operative_system == "win":
+                    input(" %s%s[+]%s All done! Press enter to exit...%s" % (Style.BRIGHT, Fore.GREEN, Fore.WHITE, Style.RESET_ALL))
+                else:
+                    print(" %s%s[+]%s All done! Exiting...%s" % (Style.BRIGHT, Fore.GREEN, Fore.WHITE, Style.RESET_ALL))
                 exit(1)
-            os.system("echo.> u_temp/" + number + ".jpg")
-            PATH = "u_temp/" + number + ".jpg"
+            os.system("echo.> u_temp/" + number + image_type)
+            PATH = "u_temp/" + number + image_type
             open(str(PATH), "wb").write(r.content)
-            print(f" [{Style.BRIGHT}{Fore.GREEN}-{Fore.WHITE}] " + THIS_IMG + " downloaded!")
+            print(f" [{Style.BRIGHT}{Fore.GREEN}-{Fore.WHITE}] {Style.RESET_ALL}{Style.DIM}{Fore.WHITE}" + THIS_IMG + f"{Style.RESET_ALL}{Style.BRIGHT}{Fore.WHITE} downloaded!")
         except Exception as e:
             print(" %s%s[!] An error ocurred: %s%s" % (Style.BRIGHT, Fore.RED, Style.RESET_ALL, e))
             print()
